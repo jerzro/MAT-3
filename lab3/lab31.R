c745d0fc0573c2c4b9893f752a2faac18e158aad
Jerzy Różański <jerzro@users.noreply.github.com>
2018-05-06 09:36:08 +0200

    Delete lab31.R

--- lab3/lab31.R
+++
@@ -1,6 +0,0 @@
-boxplot(x~y);
-aggregate(x~y, data=lab1, FUN="quantile");
-h=hist(x);
-h$counts; #szereg rozdzielczy
-rp=hist(x, freq=FALSE, breaks=60); #plot(density(x), xlim = c(1, 100));
-plot(ecdf(x));
